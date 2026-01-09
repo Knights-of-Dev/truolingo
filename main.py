e78e210d21978c91634b0909b144825b3291e3c4
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2026-01-09 10:11:05 -0500

    stops the music

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 import pygame
 import os
+import sys
+
+def fullpath(dire: str, filename: str):
+    a = os.path.join(dire, filename)
+    assert os.path.isfile(a), f"File not found at {dire}"
+    return a
+
+def close():
+    pygame.mixer.music.stop()
+    pygame.quit()
+    root.destroy()
+    sys.exit()
 
 root = tk.Tk()
+root.protocol('WM_DELETE_WINDOW', close)
 root.title("truolingo")
 embed = tk.Frame(root, width=640, height=360)
 embed.pack()
@@ -19,7 +32,7 @@ except Exception as e:
 root.update()
 
 pygame.init()
-pygame.mixer.music.load('/assets/ogg/main_menu.ogg')
+pygame.mixer.music.load(fullpath('assets/ogg', 'main_menu.ogg'))
 pygame.mixer.music.play(-1)
 screen = pygame.display.set_mode((640, 360))
 
@@ -27,6 +40,8 @@ def pygame_loop():
     screen.fill((0, 25, 0))
     pygame.display.flip()
     root.after(20, pygame_loop)
+
+running = True
 
 pygame_loop()
 root.mainloop()
